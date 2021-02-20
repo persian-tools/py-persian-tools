a318e03879794c7d6e4ebb3627d350d141c78705
ali madihi <alimadihib@gmail.com>
2021-02-20 14:01:10 +0330

    add: installable files

--- persian_tools/__init__.py
+++ persian_tools/__init__.py
@@ -1,3 +1,8 @@
+__author__ = 'Ali Madihi (mrunderline)'
+__version__ = '0.0.1'
+__date__ = '2021-02-20'
+
+
 class LANGUAGES:
     EN = 'en'
     FA = 'fa'
---
+++ setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+from importlib import import_module
+
+
+def get_long_description():
+    readme = open("README.rst").read()
+    return "\n\n".join(readme)
+
+
+setup(
+    name='persian_tools',
+    packages=['persian_tools'],
+    version=import_module('persian_tools').__version__,
+    license='MIT',
+    description='An anthology of a variety of tools for the Persian language in Python',
+    long_description=get_long_description(),
+    author='Ali Madihi (mrunderline)',
+    author_email='alimadihib@gmail.com',
+    url='https://github.com/persian-tools/py-persian-tools',
+    keywords=['digits', 'commas', 'iranian bank', 'card number', 'national id', 'national code', 'ordinal suffix'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Utilities'
+    ],
+)
