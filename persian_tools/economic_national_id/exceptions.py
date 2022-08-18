3ad18b1049d0b5c08e5c0a2b7455b872a6f9a4eb
mabedis <evil.spirit88@gmail.com>
2022-08-18 20:25:13 +0430

    Delete exceptions.py
    Delete unused exception class

--- persian_tools/economic_national_id/exceptions.py
+++
@@ -1,5 +0,0 @@
-class InvalidEconomicNationalId(Exception):
-    def __init__(self, economic_national_id):
-        self.card_number = economic_national_id
-        self.message = economic_national_id + ' is an invalid economic national id.'
-        super().__init__(self.message)
