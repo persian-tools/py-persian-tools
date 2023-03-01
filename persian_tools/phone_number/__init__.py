281cb00cb7b29734ec0642082bbf50196d9cf69b
mrunderline <alimadihib@gmail.com>
2023-03-01 12:47:41 +0330

    feat: normalize phone number, complete operators data

--- persian_tools/phone_number/__init__.py
+++ persian_tools/phone_number/__init__.py
@@ -1,16 +1,19 @@
 import re
 from typing import Union
 from .operators import data as operators_data
-from .exceptions import InvalidPhoneNumber
+from .exceptions import InvalidPhoneNumber, InvalidToken
 
 MOBILE_REGEX = '(?:[+|0{2}]?98)?(?:0)?(\\d{3})+(\\d{3})+(\\d{4})'
+VALID_TOKENS = ['+98', '98', '0098', '0']
+MOBILE_SUFFIX_REGEX = '^(?:\+98|98|0098|0)'
 
 
 def _get_operator_data(phone_number: str) -> Union[dict, bool]:
-    found_number = re.findall(MOBILE_REGEX, phone_number)
-    if found_number:
-        prefix = found_number[0][0]
-        return operators_data.get(prefix, False)
+    phone_number_suffix = re.split(MOBILE_SUFFIX_REGEX, phone_number).pop()
+    for prefix_length in range(3, 6):
+        operator_prefix = phone_number_suffix[:prefix_length]
+        if operators_data.get(operator_prefix):
+            return operators_data.get(operator_prefix)
 
     return False
 
@@ -21,8 +24,19 @@ def validate(phone_number: str) -> bool:
 
 def operator_data(phone_number: str) -> bool:
     res = _get_operator_data(phone_number)
-
     if res is False:
         raise InvalidPhoneNumber(phone_number)
 
     return res
+
+
+def normalize(phone_number: str, token: str) -> str:
+    if token not in VALID_TOKENS:
+        raise InvalidToken(token)
+
+    valid = validate(phone_number)
+    if valid is False:
+        raise InvalidPhoneNumber(phone_number)
+
+    phone_number_suffix = re.split(MOBILE_SUFFIX_REGEX, phone_number).pop()
+    return token + phone_number_suffix
