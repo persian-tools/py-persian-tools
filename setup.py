0ce1b526b5deb054d6e035f22d502b82d51140f4
underline <alimadihib@gmail.com>
2025-11-19 11:01:37 +0330

    fix: upgrade python versions

--- setup.py
+++ setup.py
@@ -23,12 +23,13 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         'Topic :: Utilities'
     ],
 )
